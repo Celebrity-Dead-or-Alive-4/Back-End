350d682729f2f02f62ffa849dffd730c9736b5a3
Joshua <cowley.josh@gmail.com>
2019-12-18 22:53:54 -0500

    Database created

--- database/migrations/20191218223245_users.js
+++ database/migrations/20191218223245_users.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+      users.increments();
   
-};
-
-exports.down = function(knex) {
+      users
+        .string('username', 255)
+        .notNullable()
+        .unique();
   
-};
+      users.string('password', 255).notNullable();
+  
+      users
+        .string('email', 255)
+        .notNullable()
+        .unique();
+  
+      users.string('date_created', 255).notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  
